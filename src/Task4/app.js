5ddb20b551dc6cb55bba7786039af56ca2ec6697
Abdul Hanan butt <buttabdulhannan708@gmail.com>
2024-11-08 19:25:01 +0500

    add reset email and password api

--- src/Task4/app.js
+++ src/Task4/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("./model/model");
+const validator = require("validator");
 const connectDb = require("./Db_Connection/Db_connection");
 
 const app = express();
@@ -64,9 +65,10 @@ app.post("/api/signIn", async (req, res) => {
 
 // Update Profile
 app.put("/api/updateProfile/:id", async (req, res) => {
-  const { fullName, email, phoneNumber } = req.body;
+  console.log(req.body);
+  const { fullName, email, phoneNumber, password } = req.body;
 
-  if (!fullName && !email && !phoneNumber) {
+  if (!fullName && !email && !phoneNumber && !password) {
     return res
       .status(400)
       .json({ message: "At least one field is required to update profile" });
@@ -78,10 +80,13 @@ app.put("/api/updateProfile/:id", async (req, res) => {
 
     if (fullName) user.fullName = fullName;
     if (email) user.email = email;
+    if (password) user.password = await bcrypt.hash(password, 10);
     if (phoneNumber) user.phoneNumber = phoneNumber;
 
     await user.save();
-    return res.status(200).json({ message: "Profile updated successfully" });
+    return res
+      .status(200)
+      .json({ message: "Profile filed updated successfully" });
   } catch (error) {
     return res
       .status(500)
@@ -105,6 +110,76 @@ app.delete("/api/delete/:id", async (req, res) => {
         .status(500)
         .json({ message: "Server Error", error: error.message });
     }
+  }
+});
+
+app.put("/api/resetPassword", async (req, res) => {
+  const { password, newPassword, userId } = req.body;
+  console.log(userId);
+  if (!password && !newPassword && !userId) {
+    return res.status(400).json({ message: "All Fileds Are Required" });
+  } else {
+    try {
+      const user = await User.findById(userId);
+      if (!user) return res.status(404).json({ message: "User not found" });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(404).json({ message: "Invalid Old Password" });
+      } else {
+        if (newPassword.length >= 8) {
+          user.password = await bcrypt.hash(newPassword, 10);
+          await user.save();
+          return res
+            .status(200)
+            .json({ message: "Password Reset Successfully" });
+        } else {
+          return res
+            .status(404)
+            .json({ message: "New Password Must Be 8 Digits Long" });
+        }
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Internal Server Error ${error}` });
+    }
+  }
+});
+
+app.put("/api/resetEmail", async (req, res) => {
+  const { password, newEmail, userId } = req.body;
+
+  if (!password || !newEmail || !userId) {
+    return res.status(400).json({ message: "All Fields Are Required" });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid Password" });
+    }
+
+    if (!validator.isEmail(newEmail)) {
+      return res.status(400).json({ message: "Invalid Email Format" });
+    }
+    const alreadyEmail = await User.findOne({ email: newEmail });
+    if (alreadyEmail) {
+      return res.status(400).json({ message: "Email Already Registered" });
+    }
+    user.email = newEmail;
+    await user.save();
+
+    return res.status(200).json({ message: "Email Reset Successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Internal Server Error: ${error.message}` });
   }
 });
 
