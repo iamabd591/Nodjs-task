1dfb29d3e6d6474661babf07df708a98cc517e5d
Abdul Hanan butt <buttabdulhannan708@gmail.com>
2024-11-10 00:03:32 +0500

    update index file for task 5

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const dotenv = require("dotenv").config();
-const app = require("./src/Task4/app")
-const PORT = process.env.PORT || 4000;
+// const app = require("./src/Task4/app")
+const express = require("express");
+const dotenv = require("dotenv").config;
+const userRouter = require("./src/Task5/router/router");
+const connectdb = require("./src/Task5/database/mongodb");
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT ${PORT}`);
+const PORT = process.env.PORT || 4000;
+app.listen(3002, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectdb()
+    .then(() => console.log("Database Connected"))
+    .catch((e) => console.log(`Error in connecting to db ${e.message}`));
 });
+app.use(express.json());
+app.use(userRouter);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on PORT ${PORT}`);
+// });
